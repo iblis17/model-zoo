5c4ddc19423a31429566560155bbdbd10b1771df
Iblis Lin <iblis@hs.ntnu.edu.tw>
2018-01-07 02:28:36 +0800

    CLArray

--- mnist/mlp.jl
+++ mnist/mlp.jl
@@ -7,14 +7,32 @@ y = onehotbatch(y, 0:9)
 
 m = Chain(
   Dense(28^2, 32, relu),
-  Dense(32, 10),
+  Dense(32, 10, σ),
   softmax)
 
 # using CuArrays
 # x, y = cu(x), cu(y)
 # m = mapleaves(cu, m)
 
-loss(x, y) = crossentropy(m(x), y)
+using CLArrays.CLArray
+
+cl2(xs) = NNlib.adapt(CLArray{Float32}, xs)
+
+NNlib.adapt_(::Type{<:CLArray{T}}, xs::AbstractArray{<:Real}) where T<:AbstractFloat =
+  isbits(xs) ? xs : convert(CLArray{T}, xs)
+
+NNlib.adapt_(::Type{<:CLArray}, xs::AbstractArray) =
+  isbits(xs) ? xs : convert(CLArray, xs)
+
+function NNlib.softmax(xs::CLArrays.CLArray)
+  ys = exp.(xs)
+  ys ./ sum(ys)
+end
+
+x, y = cl2(x), cl2(hcat(collect.(collect(y.data))...))
+m = mapleaves(cl2, m)
+
+loss(x, y) = Flux.mse(m(x), y)
 
 accuracy(x, y) = mean(argmax(m(x)) .== argmax(y))
 
@@ -22,7 +40,7 @@ dataset = repeated((x, y), 200)
 evalcb = () -> @show(loss(x, y))
 opt = ADAM(params(m))
 
-Flux.train!(loss, dataset, opt, cb = throttle(evalcb, 5))
+Flux.train!(loss, dataset, opt, cb = [evalcb])
 
 accuracy(x, y)
 
